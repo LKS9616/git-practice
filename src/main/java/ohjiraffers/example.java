e83cc2cb7995992ab7866f7f52e9672a174d0013
LKS9616 <lks609591@gmail.com>
2024-05-10 10:56:03 +0900

    first commit

---
+++ src/main/java/ohjiraffers/example.java
@@ -0,0 +1,9 @@
+package ohjiraffers;
+
+public class example {
+    public static void main(String[] args)  {
+
+        System.out.print("Helllo World!");
+        System.out.print("한글은 안 깨져요");
+    }
+}
